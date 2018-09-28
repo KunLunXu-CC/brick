32a081ce7cbe00ee386196110a2db881bc278420
qianyin925 <884820152@qq.com>
2018-09-28 13:06:56 +0800

    init

---
+++ components/DoubleColumn/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class DoubleColumn extends React.Component{
+  render(){
+    return (
+      <div>
+        双栏布局
+      </div>
+    );
+  }
+}
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import DoubleColumn from './components/DoubleColumn';
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <DoubleColumn></DoubleColumn>
+      </div>
+    );
+  }
+}
+
+export default App;
