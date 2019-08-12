56542c8ca3eaad9e953acb9e06cc2953dd5b344f
qianyin925 <qianyin95@gmail.com>
2019-08-12 17:06:59 +0800

    feat: 【qyrc】可变容器demo 添加更多演示

--- components/variable-block/api.md
+++ components/variable-block/api.md
@@ -2,17 +2,17 @@
 
 ## 一、 API 文档
 
-### 1.1 threshold
+### 1.1 operationList
 
-- 描述: 边界设置, 在指定边界距离内才允许进行操作
-- 类型: Number
-- 默认值: 5
+- 描述: 容器允许操作列表，可选值有: `top, left, right, bottom, leftTop, rightTop, leftBottom, rightBottom, drag`
+- 类型: String[]
+- 默认值: `['top', 'left', 'right', 'bottom', 'leftTop', 'rightTop', 'leftBottom', 'rightBottom', 'drag']`
 
-### 1.2 constraintSize
+### 1.2 margin
 
-- 描述: 限制大小，设置容器的最小宽度、高度
+- 描述: 容器距外容器最小外边距
 - 类型: Object, 其中所有属性只支持数字或者百分比字符串, 例如: `50 、 '50%'`
-- 默认值:  `{ width: 200, height: 200 }`
+- 默认值: { left: 0, right: 0, top: 0, bottom: 0 }
 
 ### 1.3 defaultParams
 
@@ -20,31 +20,37 @@
 - 类型: Object, 其中相关属性理论上是支持所有单位但还是推荐使用数字或者百分比字符串, 例如: `50 、 '50%'`
 - 默认值: { width: 200, height: 200, offsetX: 0, offsetY: 0 }
 
-### 1.4 dragHeight
+### 1.4 constraintSize
+
+- 描述: 限制大小，设置容器的最小宽度、高度
+- 类型: Object, 其中所有属性只支持数字或者百分比字符串, 例如: `50 、 '50%'`
+- 默认值:  `{ width: 200, height: 200 }`
+
+### 1.5 params
+
+- 描述: 容器参数, 结合 on Resize 双向绑定数据
+- 类型: Object, 其中所有属性只支持数字或者百分比字符串, 例如: `50 、 '50%'`
+- 默认值: ---
+
+### 1.6 threshold
+
+- 描述: 边界设置, 在指定边界距离内才允许进行操作
+- 类型: Number
+- 默认值: 5
+
+### 1.7 dragHeight
 
 - 描述: 顶部可拖拽区域高度，限制 modal 拖拽的区域
 - 类型: Number
 - 默认值: 20
 
-### 1.5 operationList
-
-- 描述: 容器允许操作列表，可选值有: `top, left, right, bottom, leftTop, rightTop, leftBottom, rightBottom, drag`
-- 类型: String[]
-- 默认值: `['top', 'left', 'right', 'bottom', 'leftTop', 'rightTop', 'leftBottom', 'rightBottom', 'drag']`
-
-### 1.6 margin
-
-- 描述: 容器距外容器最小外边距
-- 类型: Object, 其中所有属性只支持数字或者百分比字符串, 例如: `50 、 '50%'`
-- 默认值: { left: 0, right: 0, top: 0, bottom: 0 }
-
-### 1.7 onResize
+### 1.8 onResize
 
 - 描述: params 改变时触发函数，初始化时也会执行一次
 - 类型: Function ({ offsetX, offsetY, width, height })=>{}
 - 默认值: ---
 
-### 1.8 style
+### 1.9 style
 
 说明: 在些特殊的情况下需要覆盖 width height transform 等样式, 比如在实现某些可拉伸区块布局中则需要覆盖这些特定样式
 
@@ -52,16 +58,10 @@
 - 类型: Object
 - 默认值: ---
 
-### 1.9 className
+### 1.10 className
 
 - 描述: 追加 className
 - 类型:  String
-- 默认值: ---
-
-### 1.10 params
-
-- 描述: 容器参数, 结合 on Resize 双向绑定数据
-- 类型: Object, 其中所有属性只支持数字或者百分比字符串, 例如: `50 、 '50%'`
 - 默认值: ---
 
 ## 三、 TODO
--- stories/pages/container/VariableBlock/index.jsx
+++ stories/pages/container/VariableBlock/index.jsx
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from 'react';
 import { VariableBlock } from '@components';
 import '@components/variable-block/style';
 
-export default () => {
+// 1. 在容器内使用
+const Container = () => {
   const [params, setParams] = useState(null);
 
   const onResize = (params) => {
@@ -14,8 +15,16 @@ export default () => {
 
   return (
     <div>
-      <div onClick={() => {setParams({ width: 400 })}}>最大</div>
-      <div style={{ background: 'rgba(255, 0, 0, 0.1)', width: '800px', height: '500px' }}>
+      <div style={{textAlign: 'center'}} onClick={() => {setParams({ width: 400 })}}>最大</div>
+      <div 
+        style={{ 
+          margin: 50, 
+          width: '800px', 
+          height: '500px', 
+          overflow: 'hidden',
+          background: 'rgba(255, 0, 0, 0.1)', 
+        }}
+      >
         <VariableBlock 
           params={params} 
           onResize={onResize}
@@ -26,6 +35,46 @@ export default () => {
           </div>
         </VariableBlock>
       </div>
+    </div>
+  );
+};
+
+// 2. layout 在布局中使用
+const Layout = () => {
+  return (
+    <div style={{ display: 'flex', width: 800, height: 400, background: '#eee' }}>
+      <VariableBlock 
+        operationList={['right']}
+        margin={{ right: '20%' }}
+        style={{ height: '100%', transform: 'translate(0px, 0px)' }}
+      >
+        左侧
+      </VariableBlock>
+      <div style={{ flex: 1, background: 'pink', display: 'flex', flexDirection: 'column' }}>
+        <div style={{ flex: 1, background: '#1a1a1a' }}>
+          顶部
+        </div>
+        <VariableBlock 
+          margin={{ top: 50 }}
+          operationList={['top']}
+          defaultParams={{ height: 100 }}
+          constraintSize={{ height: 100 }}
+          style={{ width: '100%', transform: 'translate(0px, 0px)' }}
+        >
+          底部
+        </VariableBlock>
+      </div>
+    </div>
+  );
+}
+
+export default () => {
+  return (
+    <div style={{ margin: '100px 0' }}>
+      <h2>在特定容器内使用</h2>
+      <Container />
+      <h2>在布局中使用</h2>
+      <Layout />
     </div>
   );
 };
