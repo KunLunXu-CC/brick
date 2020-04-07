b9153060d15c1ee907c5973032d6cba8698b2357
qianyin925 <qianyin95@gmail.com>
2020-04-07 10:48:48 +0800

    feat: 引入 qy-create-app

---
+++ src/config/index.js
@@ -0,0 +1,17 @@
+// const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+module.exports = {
+  logo: { img: void 0, title: '练习室' },
+  iconFont: '//at.alicdn.com/t/font_1732251_st9gwlsozhd.js',
+  menu: [
+    {
+      url: '/',
+      key: 'home',
+      title: '首页',
+      icon: 'HomeOutlined',
+      router: [
+
+      ],
+    },
+  ],
+};
