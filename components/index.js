3832244db9297dbe0154e56150bafbc7fbcaf6c7
qianyin925 <qianyin925@qq.com>
2020-08-20 11:35:36 +0800

    refactor: 【qyrc】移除  scroll 同时 client 更新最新的依赖包

--- components/index.js
+++ components/index.js
@@ -2,7 +2,6 @@ export { default as VariableContainer } from './variable-container';
 export { default as CodeEditor } from './code-editor';
 export { default as Markdown } from './markdown';
 export { default as Resize } from './resize';
-export { default as Scroll } from './scroll';
 export { default as Window } from './window';
 export { default as Image } from './image';
 export { default as Icon } from './icon';
