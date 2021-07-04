d1a71d0310f68b731b158b4dd7f0f2abe61608e5
qianyin925 <qianyin925@qq.com>
2021-07-04 15:29:58 +0800

    feat: 【qyrc】markdow-preview 优化

--- components/markdown-preview/Code/Mermaid.jsx
+++ components/markdown-preview/Code/Mermaid.jsx
@@ -1,1 +1,31 @@
-export default () => (222222);
+import React from 'react';
+import mermaid from 'mermaid';
+
+// 错误处理
+mermaid.parseError = (err, hash) => console.log(
+  '%c [ markdown-preview mermaid 解析错误  ]', 'font-size:13px; background:pink; color:#bf2c9f;',
+  err,
+  hash,
+);
+
+const useHooks = props => {
+  const mermaidId = React.useRef(
+    'mermaid-x-x-x-x'.replace(/x/ig, () => Math.ceil(new Date().getTime() * Math.random()))
+  );
+
+  React.useEffect(() => {
+    mermaid.init({  }, `.${mermaidId.current}`);
+  }, [mermaidId, props.value]);
+
+  return { mermaidId };
+};
+
+export default React.memo(props => {
+  const state = useHooks(props);
+
+  return (
+    <div className={`${state.mermaidId.current} qyrc-markdown-preview-mermaid`}>
+      {props.value}
+    </div>
+  );
+});
--- src/pages/common/MarkdownPreview/test.md
+++ src/pages/common/MarkdownPreview/test.md
@@ -30,6 +30,28 @@ graph TD;
     C-->D;
 ```
 
+```mermaid
+sequenceDiagram
+    participant Alice
+    participant Bob
+    Alice->>John: Hello John, how are you?
+    loop Healthcheck
+        John->>John: Fight against hypochondria
+    end
+    Note right of John: Rational thoughts<br/>prevail...
+    John-->>Alice: Great!
+    John->>Bob: How about you?
+    Bob-->>John: Jolly good!
+```
+
+```mermaid
+graph T
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+```
+
 ## 浏览器支持
 
 ![浏览器支持](http://img.redocn.com/sheji/20141219/zhongguofengdaodeliyizhanbanzhijing_3744115.jpg)
