239fab50f6733dbadc68aa14b77f9c68bd08896f
MoYuanJun <moyuanjun925@gmail.coml>
2023-10-27 09:41:10 +0800

    feat: 🎸 update md copy

--- components/markdown/Preview/index.jsx
+++ components/markdown/Preview/index.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import Code from './Code';
 import classNames from 'classnames';
 import MarkdownToJsx from 'markdown-to-jsx';
-import getAllCSS from '../../utils/getAllCSS';
 import React, { useCallback, useMemo, useRef, useState } from 'react';
 import { Icon } from '../..';
 
@@ -29,6 +28,25 @@ const RESET_CSS = `
   }
 `;
 
+// 获取样式规则
+const getCSSRules = () => {
+  const currentRules = [...document.styleSheets]
+    .reduce((total, styleSheet) => {
+      const list = [...styleSheet.cssRules].map((rule) => {
+        if (/brick-markdown-preview/.test(rule.cssText)) {
+          return rule.cssText;
+        }
+
+        return '';
+      });
+      return [...total, ...list];
+    }, [])
+    .filter(Boolean)
+    .join('\n');
+
+  return `${currentRules}\n${RESET_CSS}`;
+};
+
 const Markdown = (props) => {
   const previewRef = useRef();
   const [copying, setCoping] = useState(false);
@@ -38,13 +56,12 @@ const Markdown = (props) => {
     // 下面代码执行时间比较长, 会阻塞 UI, 放在另一个宏任务中执行
     setTimeout(async () => {
       const sourceHtml = previewRef.current.outerHTML;
-      const css = getAllCSS();
 
       const options = {
-        removeStyleTags: true,     // 移除 Style 标签(暂时无法生效, 已有人提交 issues: https://github.com/Automattic/juice/issues/470)
+        removeStyleTags: true,      // 移除 Style 标签(暂时无法生效, 已有人提交 issues: https://github.com/Automattic/juice/issues/470)
         inlinePseudoElements: true, // 是否将伪元素转为 span 标签
       };
-      const transform = juice(`<style>${css}${RESET_CSS}</style>${sourceHtml}`, options);
+      const transform = juice(`<style>${getCSSRules()}</style>${sourceHtml}`, options);
       const replaceHtml = transform
         .replace(/<style>[\s\S]*?<\/style>/, '') // 先手动移除 Style
         .replace(/(?<=\/?)div(?=[\s>])/ig, 'section'); // div 标签转为 section
